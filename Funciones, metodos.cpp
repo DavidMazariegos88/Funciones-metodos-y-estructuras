7ab1241564e979577e6214f50cdf62472782803c
Davidmazariegos88 <jmazariegoso5@miumg.edu.gt>
2025-03-29 18:53:39 -0600

    funciones y metodos

--- Funciones, metodos.cpp
+++ Funciones, metodos.cpp
@@ -9,21 +9,66 @@ using namespace std;
 // crean una funcion o un metodo = y manda a llamar 10 veces = 80 lineas de codigo y 10 llamadas a funciones
 
 //funciones que sume dos vales
-int suma(int num1,int num2){
+/*int suma(int num1,int num2){
 	int resultado = 0;
 	resultado = num1+num2;
 	return resultado;
 }
+int resta(int num1,int num2){
+	int resultado = 0;
+	resultado = num1-num2;
+	return resultado;
+}
+*/
+
+/*int suma(int num1,int num2){
+	int resultado = 0;
+	resultado = num1+num2;
+	return resultado;
+}
+int suma(int num1,int num2,int num3){
+	int resultado = 0;
+	resultado = num1+num2+num3;
+	return resultado;
+}
+*/
+
+//funcion que sume dos valores
+
+int suma(int num1,int num2,int num3);
+void suma(int &num1,int &num2);
+
 main() {
+	int a=10,b=20;	
+	suma(a,b);
+	cout<<"a: "<<a<<endl;
+	cout<<"b: "<<b<<endl;
 	
-	cout<<suma(10,20)<<endl;
+	system("pause");
+}	
+	
+	
+	int suma(int num1,int num2,int num3){
+	int resultado = 0;
+	resultado = num1+num2+num3;
+	return resultado;
+}
+//metodo y envio de parametros por referencia
+void suma(int &num1,int &num2){
+	int resultado = 0;
+	num1+=1; //a cada dato se le suma 1
+	num2+=1; // a cada dato le suma 1
+	resultado = num1+num2; //32
+	cout<<resultado<<endl;
+}
+/*	cout<<suma(10,20)<<endl;
 	cout<<suma(50,70)<<endl;
 	cout<<suma(90,10)<<endl;
+*/	
 	
-	system("pause");
+/*	cout<<suma(10,20,20)<<endl;
+*/		
 
 
 
 
-	system("pause");
-}
