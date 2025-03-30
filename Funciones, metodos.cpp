1fbc7e7c769cca9cf3b55e4f3de36cc23c6fe18e
Davidmazariegos88 <jmazariegoso5@miumg.edu.gt>
2025-03-29 18:02:28 -0600

    primer ejemplo de funciones, sumas simples

---
+++ Funciones, metodos.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// metodos y funciones = ejecutan una porcion segun N cantidad de veces segun se mande a llamar
+// metodo = nor retorna un valor
+// funcion = retorna un tipo de dato determinado
+
+// 80 lineas de codigo en 10 lugares distintos = 800 lineas de codigo
+// de las cuales 80 son iguales
+// crean una funcion o un metodo = y manda a llamar 10 veces = 80 lineas de codigo y 10 llamadas a funciones
+
+//funciones que sume dos vales
+int suma(int num1,int num2){
+	int resultado = 0;
+	resultado = num1+num2;
+	return resultado;
+}
+main() {
+	
+	cout<<suma(10,20)<<endl;
+	cout<<suma(50,70)<<endl;
+	cout<<suma(90,10)<<endl;
+	
+	system("pause");
+
+
+
+
+	system("pause");
+}
